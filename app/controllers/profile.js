4d40843270db0a147653c87e1cddd6295923a643
bhavya121999 <bhavyagupta1203@gmail.com>
2019-08-20 12:35:03 +0530

    Applied indentation and refactored code

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -1,100 +1,88 @@
-	import Controller from '@ember/controller';
-	import Ember from 'ember';
+import Controller from '@ember/controller';
+import Ember from 'ember';
 
-	export default Controller.extend({
-	userId:"",
-	address:"",
-	currentCity:"",
-	hometown:"",
-	landmark:"",
-	pincode:"",
-	educaton:"",
-	highSchool:"",
-	college:"",
-	sociallink:"",
-	language:"",
-	about:"",
-	other:"",
-	hobbies:"",
-	skills:"",
-	music:"",
-	programmes:"",
-	team:"",
-	people:"",
-	quotes:"",
-	event:"",
-
-
-	actions:{
-		doCreate() {
-		// const userRecord=	this.store.createRecord('user', {
-	// 			email: this.get("email"),
-	// 			userName: this.get("userName"),
-	// 			password: this.get("password"),
+export default Controller.extend({
+userId:"",
+address:"",
+currentCity:"",
+hometown:"",
+landmark:"",
+pincode:"",
+educaton:"",
+highSchool:"",
+college:"",
+sociallink:"",
+language:"",
+about:"",
+other:"",
+hobbies:"",
+skills:"",
+music:"",
+programmes:"",
+team:"",
+people:"",
+quotes:"",
+event:"",
 
 
-	// 			password: "test"
-		// });
-	//       const currentTime = moment.unix("1565856526").format('dddd, MMMM Do, YYYY h:mm:ss A');
-		// const selectedDatePickerTime = moment.utc(this.get("expiresAt"), "ddd MMMM Do YYYY h:mm:ss A").unix();
+actions:{
+doCreate() {
+const self = this;
+const dataRecord = {
+					"userName" :  this.get("userId"),
+					"address":this.get("address"),
+                    "currentCity":this.get("currentCity"),
+                    "hometown":this.get("hometown"),
+                    "landmark":this.get("landmark"),
+                    "pincode":this.get("pincode"),
+                    "educaton":this.get("educaton"),
+                    "highSchool":this.get("highSchool"),
+                    "college":this.get("college"),
+                    "sociallink":this.get("sociallink"),
+                    "language":this.get("language"),
+                    "about":this.get("about"),
+                    "other":this.get("other"),
+                    "hobbies":this.get("hobbies"),
+                    "skills":this.get("skills"),
+                    "music":this.get("music"),
+                    "programmes":this.get("programmes"),
+                    "team":this.get("team"),
+                    "people":this.get("people"),
+                    "quotes":this.get("quotes"),
+                    "event":this.get("event")
+				   }
+
+var fileInput = document.getElementById('input');
+var file = fileInput.files[0];
+var formData = new FormData();
+formData.append('file', file);
+///formData.append('filetype',file.type)
+var xhr = new XMLHttpRequest();
+const url = "http://localhost:9092"
+// Add any event handlers here...
+xhr.open('POST',url + "/image/upload", true);
+xhr.send(formData);
+Ember.$.ajax
+	({
+    type: "POST",
+    url: url + "/users/20",
+    contentType: "application/json;charset=utf-8",
+    data: JSON.stringify(dataRecord),
+    dataType: "json",
+    success: function(data) {
+     self.set("test", true);
+    },
+    error: function(xhr, error){
+       self.set("test", true);
+    } 
+    
+}) 
 
 
-		const self = this;
-		const dataRecord = {
-							"userName" :  this.get("userId"),
-							"address":this.get("address"),
-	                        "currentCity":this.get("currentCity"),
-	                        "hometown":this.get("hometown"),
-	                        "landmark":this.get("landmark"),
-	                        "pincode":this.get("pincode"),
-	                        "educaton":this.get("educaton"),
-	                        "highSchool":this.get("highSchool"),
-	                        "college":this.get("college"),
-	                        "sociallink":this.get("sociallink"),
-	                        "language":this.get("language"),
-	                        "about":this.get("about"),
-	                        "other":this.get("other"),
-	                        "hobbies":this.get("hobbies"),
-	                        "skills":this.get("skills"),
-	                        "music":this.get("music"),
-	                        "programmes":this.get("programmes"),
-	                        "team":this.get("team"),
-	                        "people":this.get("people"),
-	                        "quotes":this.get("quotes"),
-	                        "event":this.get("event")
-						   }
-
-	var fileInput = document.getElementById('input');
-	var file = fileInput.files[0];
-	var formData = new FormData();
-	formData.append('file', file);
-	///formData.append('filetype',file.type)
-	var xhr = new XMLHttpRequest();
-	const url = "http://35.154.7.11:9092"
-	// Add any event handlers here...
-	xhr.open('POST',url + "/image/upload", true);
-	xhr.send(formData);
-		Ember.$.ajax
-	  	({
-	        type: "POST",
-	        url: url + "/users/20",
-	        contentType: "application/json;charset=utf-8",
-	        data: JSON.stringify(dataRecord),
-	        dataType: "json",
-	        success: function(data) {
-	         self.set("test", true);
-	        },
-	        error: function(xhr, error){
-	           alert("error occured. Please try again")
-	        } 
-	        
-		}) 
+},
 
 
-		},
-		
-
-	}
+}
 
 
-	});
+});
